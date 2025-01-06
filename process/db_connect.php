0bb4cd6f7db74c5ad986e4894ddef38915e40c1b
MRN <mark.rencie.nol035@gmail.com>
2025-01-06 09:01:18 +0800

    update database connection

--- process/db_connect.php
+++ process/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "adserver.cmpxqpkk4nrk.ap-southeast-1.rds.amazonaws.com";
-$username = "admin";
-$password = "ArtBGl0bal123!";
-$dbname = "artb_ad_db";
+$username = "devy";
+$password = "admin2024!";
+$dbname = "BLCKWINFUN";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
